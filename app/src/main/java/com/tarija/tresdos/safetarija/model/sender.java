2686f4c86148117965e2ae14654c1d246d965691
Luis Juarez <tresdosasd@gmail.com>
2017-10-13 14:11:27 -0400

    Add more class

---
+++ app/src/main/java/com/tarija/tresdos/safetarija/model/sender.java
@@ -0,0 +1,10 @@
+package com.tarija.tresdos.safetarija.model;
+
+/**
+ * Created by Tresdos on 10/13/2017.
+ */
+
+public class sender {
+    public String to;
+    public Notification notification;
+}
