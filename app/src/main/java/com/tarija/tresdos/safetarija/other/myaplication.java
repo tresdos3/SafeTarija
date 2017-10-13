16139898884cf69a96e29b7fa5c5eb45416b0b82
Luis Juarez <tresdosasd@gmail.com>
2017-10-13 17:21:13 -0400

    Track Service in Work V2

--- app/src/main/java/com/tarija/tresdos/safetarija/other/myaplication.java
+++ app/src/main/java/com/tarija/tresdos/safetarija/other/myaplication.java
@@ -1,16 +1,16 @@
 package com.tarija.tresdos.safetarija.other;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.tarija.tresdos.safetarija.receiver.NetworkReceiver;
 
-public class myaplication extends Application {
+public class myaplication extends MultiDexApplication {
     private static myaplication mInstance;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
         mInstance = this;
     }
 
